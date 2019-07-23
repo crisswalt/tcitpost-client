2b557bd2aae24622128d36afb395570e37030fd9
Cristian Carrasco <criswalt@gmail.com>
2019-07-23 01:38:35 -0400

    Quitando logger

--- src/store.js
+++ src/store.js
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
-const loggerMiddleware = createLogger();
 
 const store = createStore(
   rootReducer,
